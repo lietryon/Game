a7aca34ddae73d838f2567cbb1a4ea2a9f07af5e
lietryon <29160922+lietryon@users.noreply.github.com>
2020-09-08 10:17:52 +0200

    Version 0.1
    The first batch of files, documenting my progress creating a 2D platformer.
    
    - Basic level design
    - Basic movement
    - Running animation
    - Camera position changing relative to the player's position

---
+++ Assets/Caveman/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed; 
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal"); 
+        float moveBy = x * speed; 
+        rb.velocity = new Vector2(moveBy, rb.velocity.y); 
+        
+        if (x == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+
+    }
+}
---
+++ Assets/Jump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpForce;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        jump();
+    }
+
+    void jump() 
+    { 
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce); 
+        }
+    } 
+}
